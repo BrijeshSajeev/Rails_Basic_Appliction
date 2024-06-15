0763fb8616ab9b3cdb181b6c41d764b5bcbe212f
brijeshsajeev <brijesh00300@gmail.com>
2024-06-15 23:00:28 +0530

    Update the schema.db file with new column
    - Update the DB for new column using

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_15_141755) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_15_172110) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sub_title"
   end
 
 end
