afba7ed42590770f8c2d9a28b0f44f640fad8a60
brijeshsajeev <brijesh00300@gmail.com>
2024-06-15 23:07:52 +0530

    Update the Db for removing the column
    - Update the Schema using

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_15_172110) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_15_173536) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "sub_title"
   end
 
 end
