82b542386599d72ac8ca1a63850dc6a8e274006a
brijeshsajeev <brijesh00300@gmail.com>
2024-06-16 18:51:21 +0530

    Add Resourceful routing
    - Add Resource full routing using
    - It will automatically generate set of routes for a resource

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
   get 'articles/:id' , to: 'articles#show'
-  
+  #ResourceFull routing
+  resource :articles
 end
