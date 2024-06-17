b1c48b953d824bf5bfa065bc91d007b829dba604
brijeshsajeev <brijesh00300@gmail.com>
2024-06-17 19:23:13 +0530

    Create a form template using Form_bulider

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,18 +2,18 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
-  
+
   def new
     @article = Article.new
   end
 
   def create
-    @article = Article.new(article_param)
-    
+    @article = Article.new(article_params)
+
     if @article.save
       redirect_to @article
     else
@@ -22,10 +22,8 @@ class ArticlesController < ApplicationController
   end
 
   private
-    def article_param
-      params.require(:article).permit(:title,:body)
-    end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
-
-
-
